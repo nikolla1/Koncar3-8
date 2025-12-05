7579b1ac32693197778c854ef26c35834615d444
nikolla1 <simicnikola626@gmail.com>
2025-12-05 09:29:47 +0100

    Update program.c

--- program.c
+++ program.c
@@ -2,23 +2,5 @@
 int main(){
   printf("Dobro dosli na kurs Linux-a\n");
   printf("Dobro dosli na kurs Linux-a\n");
-  printf("Dobro dosli na kurs Linux-a\n");
-  printf("Dobro dosli na kurs Linux-a\n");
-  printf("Dobro dosli na kurs Linux-a\n");
-  printf("Dobro dosli na kurs Linux-a\n");
-  printf("Dobro dosli na kurs Linux-a\n");
-  printf("Dobro dosli na kurs Linux-a\n");
-  printf("Dobro dosli na kurs Linux-a\n");
-  printf("Dobro dosli na kurs Linux-a\n");
-  printf("Dobro dosli na kurs Linux-a\n");
-  printf("Dobro dosli na kurs Linux-a\n");
-  printf("Dobro dosli na kurs Linux-a\n");
-  printf("Dobro dosli na kurs Linux-a\n");
-  printf("Dobro dosli na kurs Linux-a\n");
-  printf("Dobro dosli na kurs Linux-a\n");
-  printf("Dobro dosli na kurs Linux-a\n");
-  printf("Dobro dosli na kurs Linux-a\n");
-  printf("Dobro dosli na kurs Linux-a\n");
-  printf("Dobro dosli na kurs Linux-a\n");
   return 0;
 }
